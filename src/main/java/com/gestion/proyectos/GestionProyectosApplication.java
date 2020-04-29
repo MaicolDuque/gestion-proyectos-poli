05545a015e521cf52ae36a47452ca9f7fba51765
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2020-04-29 11:24:31 -0500

    Add CORS globlally

--- src/main/java/com/gestion/proyectos/GestionProyectosApplication.java
+++ src/main/java/com/gestion/proyectos/GestionProyectosApplication.java
@@ -2,6 +2,9 @@ package com.gestion.proyectos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GestionProyectosApplication {
@@ -9,5 +12,19 @@ public class GestionProyectosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestionProyectosApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				 .allowedOrigins("*")
+				 .allowedMethods("*");
+			}
+		};
+	}
+
+
 
 }
